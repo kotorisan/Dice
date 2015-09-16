f67b39c7b2494925ef7d686fad184ea20eeb5916
unknown <stdeng1@697-334-Upbourn.sf.k12.internal>
2015-09-16 11:17:21 -0700

    umi is waifu and laifu

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -14,88 +14,128 @@ import java.io.IOException;
 
 public class Dice extends PApplet {
 
-Die sinon;
-
+int count = 0;
+int highest = 0;
+public void setup()
 {
-  size(500,500);
   noLoop();
+  size(500,650);
+  
 }
 public void draw()
 {
-  background(225);
-  sinon = new Die(x,y);
-  strokeWeight(1);
-  stroke(255);
-  fill(0);
-  rect(350,350,100,100);
-  fill(255);
-  textSize(25);
-  textAlign(CENTER);
-  text(sum,400,408);
-  //weeb nation 101 cant stop me
+  background(255,255,0);
+  //trying to make this confusing for you
+ for(int Sinon = 50;  Sinon<= 350; Sinon+=150)
+ {
+   for(int Asuna = 50; Asuna<=350; Asuna += 150)
+   {
+     new Die(Sinon,Asuna);
+   }
+ }
+fill(0,255,255);
+textSize(32);
+
+text("Total:" + count,120,525);
+text("Umi is Lyfe", 120,600);
+
+ 
 }
+
 public void mousePressed()
 {
   redraw();
+  
+  count = 0;
+ 
 }
 class Die //models one single dice cube
 {
-	int dx, dy, num;
-	Die(int x, int y) //constructor
-	{
-		dx = x;
-		dy = y;
-	}
-	public void roll()
-	{
-		num = (int)(Math.random()*6) + 1;
-	}
-	public void show()
-	{
-		noStroke();
-		fill(255);
-		rect(dx,dy,100,100,10);
-		fill(0);
-		if(num == 1)
-		{
-			ellipse(dx + 50,dy + 50,18,18);
-		}
-		else if(num == 2)
-		{
-			ellipse(dx + 75,dy + 25,18,18);
-			ellipse(dx + 25,dy + 75,18,18);
-		}
-		else if(num == 3)
-		{
-			ellipse(dx + 50,dy + 50,18,18);
-			ellipse(dx + 75,dy + 25,18,18);
-			ellipse(dx + 25,dy + 75,18,18);
-		}
-		else if(num == 4)
-		{
-			ellipse(dx + 75,dy + 25,18,18);
-			ellipse(dx + 25,dy + 75,18,18);
-			ellipse(dx + 75,dy + 75,18,18);
-			ellipse(dx + 25,dy + 25,18,18);
-		}
-		else if(num == 5)
-		{
-			ellipse(dx + 50,dy + 50,18,18);
-			ellipse(dx + 75,dy + 25,18,18);
-			ellipse(dx + 25,dy + 75,18,18);
-			ellipse(dx + 75,dy + 75,18,18);
-			ellipse(dx + 25,dy + 25,18,18);
-		}
-		else if(num == 6)
-		{
-			ellipse(dx + 75,dy + 25,18,18);
-			ellipse(dx + 25,dy + 75,18,18);
-			ellipse(dx + 75,dy + 75,18,18);
-			ellipse(dx + 25,dy + 25,18,18);
-			ellipse(dx + 25,dy + 50,18,18);
-			ellipse(dx + 75,dy + 50,18,18);
-		}
-	}
+  int side;
+  int myX;
+  int myY;
+  Die(int x, int y) //constructor
+  {
+    myX = x;
+    myY = y;
+    roll();
+    show();
+  }
+  public void roll()
+  {
+    side = (int)(Math.random()*6 + 1);
+    
+    
+  
+  }
+  
+  public void show()
+  {
+    count = count + side;
+    
+    
+    if (count > highest)
+    {
+      highest = count;
+      
+    }
+    
+    int Umi = 20;
+    //int it called umi because umi is lyfe
+    
+    fill(255);
+    rect(myX,myY,100,100);
+    
+    if(side == 1)
+    {
+      
+      fill(255,0,0);
+      ellipse(myX+50,myY+50,Umi,Umi);
+      
+    }
+    else if(side == 2)
+    {
+      
+      fill(255,0,0);
+      ellipse(myX+25,myY+25,Umi,Umi);
+      ellipse(myX+75,myY+75,Umi,Umi);
+    }
+    else if(side == 3)
+    {
+      fill(0,255,0);
+      ellipse(myX+25,myY+25,Umi,Umi);
+      ellipse(myX+75,myY+75,Umi,Umi);
+      ellipse(myX+50,myY+50,Umi,Umi);
+    }
+    else if (side == 4)
+    {
+      fill(0,255,0);
+      ellipse(myX+25,myY+75,Umi,Umi);
+      ellipse(myX+25,myY+25,Umi,Umi);
+      ellipse(myX+75,myY+75,Umi,Umi);
+      ellipse(myX+75,myY+25,Umi,Umi);
+      
+    }
+    else if (side == 5)
+    {
+      fill(0,0,255);
+      ellipse(myX+25,myY+75,Umi,Umi);
+      ellipse(myX+25,myY+25,Umi,Umi);
+      ellipse(myX+75,myY+75,Umi,Umi);
+      ellipse(myX+75,myY+25,Umi,Umi);
+      ellipse(myX+50,myY+50,Umi,Umi);
+    }
+     else if (side == 6)
+    {
+      fill(0,0,255);
+      ellipse(myX+25,myY+75,Umi,Umi);
+      ellipse(myX+25,myY+25,Umi,Umi);
+      ellipse(myX+75,myY+75,Umi,Umi);
+      ellipse(myX+75,myY+25,Umi,Umi);
+      ellipse(myX+50,myY+25,Umi,Umi);
+      ellipse(myX+50,myY+75,Umi,Umi);
+    }
+  }
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Dice" };
